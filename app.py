8e57b112f63c61ba231dd64fee96f160b4796f07
Dimmisc <dimamyscen@gmail.com>
2025-01-13 10:45:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+from flask import dashboards, Flask
+
+app = Flask(__name__)
+
+app.route("/autorisation", methods=['GET', 'POST'])
+def autorisation():
+
+app.route("/inventory")
+def inventory():
+  
+app.route("/inventory/<orderer>")
+def order():
 
