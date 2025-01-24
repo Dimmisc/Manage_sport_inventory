daf3202f69511545a5ca4c31e49a3bfc7840e17d
MonVcher <feeleejeen@gmail.com>
2025-01-24 13:19:52 +0300

    Update request.py

--- data/request.py
+++ data/request.py
@@ -7,6 +7,10 @@ from .db_session import SqlAlchemyBase
 class Request(SqlAlchemyBase):
     __tablename__ = 'request'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    id_item = sqlalchemy.relationship(
-    time = 
-    
+    description = sqlalchemy.Column(sqlalchemy.String)
+    created_date_start = sqlalchemy.Column(sqlalchemy.DateTime)
+    created_date_end = sqlalchemy.Column(sqlalchemy.DateTime)
+
+    id_user = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
+    users = orm.relationship('Users')
+    id_item = orm.relationship("Asortiment", secondary="association", backref="request")
