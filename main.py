ca2a821a35ebd7e52cb57579254b8e0629391e42
Dimmisc <dimamyscen@gmail.com>
2025-02-08 01:52:27 +0300

    Почти всё

--- main.py
+++ main.py
@@ -82,233 +82,268 @@ def logout():
     return redirect("/")
 
 
-@app.route('/Unconfirm_request/<int:id_item>', methods=['GET', 'POST'])
+@app.route('/Unconfirm_request/<int:id_request>', methods=['GET', 'POST'])
 @login_required
-def Unconfirm_item(id_item):
-    db_sess = db_session.create_session()
-    item = db_sess.query(Request).filter(Request.id == id_item).first()
-    if item:
-        db_sess.delete(item)
-        db_sess.commit()
-        update_requests(db_sess)
-    else:
+def Unconfirm_item(id_request):
+    if current_user.user_access == "baned":
         abort(404)
-    return redirect('/arrended')
+    else:
+        db_sess = db_session.create_session()
+        reques = db_sess.query(Request).filter(Request.id == id_request).first()
+        if reques:
+            db_sess.delete(reques)
+            db_sess.commit()
+            update_requests(db_sess)
+        else:
+            abort(404)
+        return redirect('/arrended')
 
 
 @app.route('/delete_item/<int:id_item>', methods=['GET', 'POST'])
 @login_required
 def delete_item(id_item):
-    db_sess = db_session.create_session()
-    item = db_sess.query(Asortiment).filter(Asortiment.id == id_item).first()
-    if item:
-        db_sess.delete(item)
-        db_sess.commit()
-        update_requests(db_sess)
-    else:
+    if current_user.user_access == "baned":
         abort(404)
-    return redirect('/admin_panel')
-
-
-@app.route("/unconfirm_request<int:id_request>")
-@login_required
-def unconfirm_request(id_request):
-    db_sess = db_session.create_session()
-    item = db_sess.query(Request).filter_by(id=id_request).first()
-    if item:
-        db_sess.delete(item)
-        db_sess.commit()
-        update_requests(db_sess)
+    elif current_user.user_access == "User":
+        return redirect("/")
     else:
-        abort(404)
-    return redirect("/arrended")
+        db_sess = db_session.create_session()
+        item = db_sess.query(Asortiment).filter(Asortiment.id == id_item).first()
+        if item:
+            db_sess.delete(item)
+            db_sess.commit()
+            update_requests(db_sess)
+        else:
+            abort(404)
+        return redirect('/admin_panel')
+
 
 @app.route('/request/<int:id_item>', methods=['GET', 'POST'])
 @login_required
 def edit_news(id_item):
-    db_sess = db_session.create_session()
-    item = db_sess.query(Asortiment).filter_by(id=id_item).first()
-    form = RequestForm()
-    if form.validate_on_submit():
-        if Check_free(form.datetime_start.data, form.datetime_end.data, id_item) == True:
-            request = Request(id_item=id_item,
-                              id_user=current_user.id,
-                              user=db_sess.query(Users).filter_by(id=current_user.id).one(),
-                              date_start=str(form.datetime_start.data),
-                              date_end=str(form.datetime_end.data),
-                              description=form.description.data,
-                              type="Не одобрен"
-                              )
-            db_sess.add(request)
-            db_sess.commit()
-            return redirect("/")
-        else:
-            return render_template("request.html",
-                                   form=form,
-                                   item=item,
-                                   message="Товар занят в указаное вами время!",
-                                   title="Аренда",
-                                   )
-    return render_template("request.html",
-                           form=form,
-                           item=item,
-                           title="Аренда",
-                           )
+    if current_user.user_access == "baned":
+        abort(404)
+    else:
+        db_sess = db_session.create_session()
+        item = db_sess.query(Asortiment).filter_by(id=id_item).first()
+        form = RequestForm()
+        if form.validate_on_submit():
+            if Check_free(form.datetime_start.data, form.datetime_end.data, id_item) == True:
+                request = Request(id_item=id_item,
+                                id_user=current_user.id,
+                                user=db_sess.query(Users).filter_by(id=current_user.id).one(),
+                                date_start=str(form.datetime_start.data),
+                                date_end=str(form.datetime_end.data),
+                                description=form.description.data,
+                                type="Не одобрен"
+                                )
+                db_sess.add(request)
+                db_sess.commit()
+                return redirect("/")
+            else:
+                return render_template("request.html",
+                                    form=form,
+                                    item=item,
+                                    message="Товар занят в указаное вами время!",
+                                    title="Аренда",
+                                    )
+        return render_template("request.html",
+                            form=form,
+                            item=item,
+                            title="Аренда",
+                            )
 
 
 @app.route("/")
 def index():
-    db_sess = db_session.create_session()
-    check_out(db_sess)
-    if current_user.is_authenticated:
-        user = db_sess.query(Users).filter_by(id=current_user.id).first()
-        item = db_sess.query(Asortiment).all()
+    if current_user.user_access == "baned":
+        abort(404)
     else:
-        item = db_sess.query(Asortiment).all()
-        user = None
-    return render_template("index.html", 
-                           news=item, 
-                           user=user, 
-                           title="Главная"
-                           )
+        db_sess = db_session.create_session()
+        check_out(db_sess)
+        if current_user.is_authenticated:
+            user = db_sess.query(Users).filter_by(id=current_user.id).first()
+            item = db_sess.query(Asortiment).all()
+        else:
+            item = db_sess.query(Asortiment).all()
+            user = None
+        return render_template("index.html", 
+                            news=item, 
+                            user=user, 
+                            title="Главная"
+                            )
 
 
 @app.route("/arrended")
 @login_required
 def arrended():
-    db_sess = db_session.create_session()
-    items = db_sess.query(Request).filter_by(id_user=current_user.id).all()
-    trtp = len(items) != 0
-    return render_template("arrended.html",
-                           items=items,
-                           trtp=trtp,
-                           title="Ваши арендованые предметы"
-                           )
+    if current_user.user_access == "baned":
+        abort(404)
+    else:
+        db_sess = db_session.create_session()
+        items = db_sess.query(Request).filter_by(id_user=current_user.id).all()
+        trtp = len(items) != 0
+        return render_template("arrended.html",
+                            items=items,
+                            trtp=trtp,
+                            title="Ваши арендованые предметы"
+                            )
 
 
 @app.route("/admin_panel")
 @login_required
 def admin_panel():
-    db_sess = db_session.create_session()
-    if current_user.user_access == "admin":
-        items = db_sess.query(Asortiment).all()
-        requests = db_sess.query(Request).all()
-        users = db_sess.query(Users).all()
-        types = db_sess.query(Idtype).all()
-        return render_template("admin_panel.html", 
-                               items=items, 
-                               requests=requests, 
-                               types=types, 
-                               users=users, 
-                               title="Управление сайтом"
-                               )
-    else:
+    if current_user.user_access == "baned":
+        abort(404)
+    elif current_user.user_access == "User":
         return redirect("/")
+    else:
+        db_sess = db_session.create_session()
+        if current_user.user_access == "admin":
+            items = db_sess.query(Asortiment).all()
+            requests = db_sess.query(Request).all()
+            users = db_sess.query(Users).all()
+            types = db_sess.query(Idtype).all()
+            return render_template("admin_panel.html", 
+                                items=items, 
+                                requests=requests, 
+                                types=types, 
+                                users=users, 
+                                title="Управление сайтом"
+                                )
+        else:
+            return redirect("/")
 
 
 @app.route("/edit_item/<int:id_item>", methods=["GET", "POST"])
 @login_required
 def edit_item(id_item):
-    db_sess = db_session.create_session()
-    form = IU()
-    if form.validate_on_submit():
-        db_sess.query(Asortiment).filter(Asortiment.id == id_item).update({'name': form.name.data, 'status': form.status.data})
-        db_sess.commit()
-        return redirect("/admin_panel")
-    item = db_sess.query(Asortiment).filter_by(id=id_item).first()
-    return render_template("edit_item.html",
-                           title="Редактирование предмета",
-                           form=form,
-                           item=item,
-                           )
+    if current_user.user_access == "baned":
+        abort(404)
+    elif current_user.user_access == "User":
+        return redirect("/")
+    else:
+        db_sess = db_session.create_session()
+        form = IU()
+        if form.validate_on_submit():
+            db_sess.query(Asortiment).filter(Asortiment.id == id_item).update({'name': form.name.data, 'status': form.status.data})
+            db_sess.commit()
+            return redirect("/admin_panel")
+        item = db_sess.query(Asortiment).filter_by(id=id_item).first()
+        return render_template("edit_item.html",
+                            title="Редактирование предмета",
+                            form=form,
+                            item=item,
+                            )
 
 
 @app.route('/add_item', methods=['GET', 'POST'])
 @login_required
 def add_item():
-    form = AsortimentForm()
-    db_sess = db_session.create_session()
-    types = db_sess.query(Idtype.name).all()
-    choices = [[types[i][0], types[i][0]] for i in range(len(types))] 
-    form.type.choices = choices
-    if form.validate_on_submit():
-        item = Asortiment()
-        item.name = form.name.data
-        item.status = form.status.data
-        img_file = secure_filename(form.photo.data.filename)
-        path = os.path.join(app.config['UPLOAD_FOLDER'], img_file)
-        form.photo.data.save(path)
-        item.photo_href = path
-        type = db_sess.query(Idtype).filter_by(name=form.type.data).first()
-        user = db_sess.query(Users).filter_by(id=current_user.id).first()
-        item.users = user
-        if type:
-            item.idtype = type
-        db_sess.add(item)
-        db_sess.commit()
-        return redirect('/admin_panel')
-    return render_template("add_item.html", form=form, title="Добавить объект")
+    if current_user.user_access == "baned":
+        abort(404)
+    elif current_user.user_access == "User":
+        return redirect("/")
+    else:
+        form = AsortimentForm()
+        db_sess = db_session.create_session()
+        types = db_sess.query(Idtype.name).all()
+        choices = [[types[i][0], types[i][0]] for i in range(len(types))] 
+        form.type.choices = choices
+        if form.validate_on_submit():
+            item = Asortiment()
+            item.name = form.name.data
+            item.status = form.status.data
+            img_file = secure_filename(form.photo.data.filename)
+            path = os.path.join(app.config['UPLOAD_FOLDER'], img_file)
+            form.photo.data.save(path)
+            item.photo_href = path
+            type = db_sess.query(Idtype).filter_by(name=form.type.data).first()
+            user = db_sess.query(Users).filter_by(id=current_user.id).first()
+            item.users = user
+            if type:
+                item.idtype = type
+            db_sess.add(item)
+            db_sess.commit()
+            return redirect('/admin_panel')
+        return render_template("add_item.html", form=form, title="Добавить объект")
 
 
 @app.route('/add_type', methods=['GET', 'POST'])
 @login_required
 def add_type():
-    form = IdtypeForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        type = Idtype(name=form.name.data,
-                      description=form.description.data,
-                      )
-        db_sess.add(type)
-        db_sess.commit()
-        return redirect("/admin_panel")
-    return render_template("add_type.html", form=form, title="Добавление типов")
+    if current_user.user_access == "baned":
+        abort(404)
+    elif current_user.user_access == "User":
+        return redirect("/")
+    else:
+        form = IdtypeForm()
+        if form.validate_on_submit():
+            db_sess = db_session.create_session()
+            type = Idtype(name=form.name.data,
+                        description=form.description.data,
+                        )
+            db_sess.add(type)
+            db_sess.commit()
+            return redirect("/admin_panel")
+        return render_template("add_type.html", form=form, title="Добавление типов")
 
 
 @app.route("/edit_user/<int:id_user>", methods=["GET", "POST"])
 @login_required
 def edit_user(id_user):
-    db_sess = db_session.create_session()
-    user = db_sess.query(Users).filter_by(id=id_user).first()  
-    form = EU()
-    if form.validate_on_submit():
-        db_sess.query(Users).filter_by(id=id_user).update({'user_access': form.access.data,
-                                                          'about': form.status.data}, 
-                                                          )
-        db_sess.commit()
-        return redirect("/admin_panel")
-    return render_template("edit_user.html", 
-                           form=form, 
-                           user=user, 
-                           title="Изменение пользователя")
+    if current_user.user_access == "baned":
+        abort(404)
+    elif current_user.user_access == "User":
+        return redirect("/")
+    else:
+        db_sess = db_session.create_session()
+        user = db_sess.query(Users).filter_by(id=id_user).first()  
+        form = EU()
+        if form.validate_on_submit():
+            db_sess.query(Users).filter_by(id=id_user).update({'user_access': form.access.data,
+                                                            'about': form.status.data}, 
+                                                            )
+            db_sess.commit()
+            return redirect("/admin_panel")
+        form.status.data = user.about
+        return render_template("edit_user.html", 
+                            form=form, 
+                            user=user, 
+                            title="Изменение пользователя")
 
 
 @app.route("/confirm_request/<int:id_request>", methods=["GET", "POST"])
 @login_required
 def confirm_request(id_request):
-    form = RequestForm()
-    db_sess = db_session.create_session()
-    if form.validate_on_submit():
-        if form.confirmed.data == True:
-            db_sess.query(Request).filter_by(id=id_request).update({'description': form.description.data,
-                                                                   'approved': form.confirmed.data,
-                                                                   'type': 'Одобрен'}
-                                                                   )
-        else:
-            db_sess.query(Request).filter_by(id=id_request).update({'description': form.description.data,
-                                                                   'approved': form.confirmed.data,
-                                                                   'type': 'Не одобрен'}
-                                                                   )
-        db_sess.commit()
-        return redirect("/admin_panel")
-    request = db_sess.query(Request).filter_by(id=id_request).first()
-    form.description.data = request.description
-    form.confirmed.data = request.approved
-    return render_template("confirm_request.html",
-                           form=form, 
-                           request=request, 
-                           title="Одобрение запроса"
-                           )
+    if current_user.user_access == "baned":
+        abort(404)
+    elif current_user.user_access == "User":
+        return redirect("/")
+    else:
+        form = RequestForm()
+        db_sess = db_session.create_session()
+        if form.validate_on_submit():
+            if form.confirmed.data == True:
+                db_sess.query(Request).filter_by(id=id_request).update({'description': form.description.data,
+                                                                    'approved': form.confirmed.data,
+                                                                    'type': 'Одобрен'}
+                                                                    )
+            else:
+                db_sess.query(Request).filter_by(id=id_request).update({'description': form.description.data,
+                                                                    'approved': form.confirmed.data,
+                                                                    'type': 'Не одобрен'}
+                                                                    )
+            db_sess.commit()
+            return redirect("/admin_panel")
+        request = db_sess.query(Request).filter_by(id=id_request).first()
+        form.description.data = request.description
+        form.confirmed.data = request.approved
+        return render_template("confirm_request.html",
+                            form=form, 
+                            request=request, 
+                            title="Одобрение запроса"
+                            )
 
 
 @app.route('/register', methods=['GET', 'POST'])
